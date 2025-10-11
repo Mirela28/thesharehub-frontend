46ca342aac1c8336691d2314571cc52513fac7bb
Mirela28 <mirelagirleanu@gmail.com>
2025-10-11 16:35:47 +0200

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import { UserProvider } from './contexts/UserContext';
+import Account from './pages/Account';
 
 function App() {
   const location = useLocation();
@@ -20,6 +21,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/account" element={<Account />} />
         </Routes>
       </UserProvider>
     </div>
